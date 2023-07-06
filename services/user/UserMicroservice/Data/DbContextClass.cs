1938e2c84f68095570313eab0d5c8b2662aaaeac
Maxim P <raiyin@ya.ru>
2023-07-06 22:56:50 +0300

    CT-3 Add model, controller, migration, config

---
+++ services/user/UserMicroservice/Data/DbContextClass.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserMicroservice.Model;
+
+namespace UserMicroservice.Data
+{
+    public class DbContextClass:DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        //protected readonly IConfiguration Configuration;
+
+        //public DbContextClass(IConfiguration configuration)
+        //{
+        //    Configuration = configuration;
+        //}
+        //protected override void OnConfiguring(DbContextOptionsBuilder options)
+        //{
+        //    options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+        //}
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+           => optionsBuilder.UseNpgsql("Host=localhost;Database=users;Username=postgres;Password=postgres;Port=6543");
+    }
+}
--- services/user/UserMicroservice/Program.cs
+++ services/user/UserMicroservice/Program.cs
@@ -1,8 +1,14 @@
+using UserMicroservice.Data;
+using UserMicroservice.Services.Interface;
+using UserMicroservice.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddDbContext<DbContextClass>();
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
