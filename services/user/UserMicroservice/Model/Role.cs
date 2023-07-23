629d146b0e4f9b2a262015f2dd4e17e0c9777163
Maxim P <raiyin@ya.ru>
2023-07-23 22:43:17 +0300

    СЕ-3 Updated user table, add roles

---
+++ services/user/UserMicroservice/Model/Role.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace UserMicroservice.Model
+{
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum Role
+    {
+        User = 1,
+        Driver = 2,
+    }
+}
--- services/user/UserMicroservice/Model/User.cs
+++ services/user/UserMicroservice/Model/User.cs
@@ -1,9 +1,18 @@
-﻿namespace UserMicroservice.Model
+﻿using System.Formats.Asn1;
+
+namespace UserMicroservice.Model
 {
     public class User
     {
         public int UserId { get; set; }
-        public string UserName { get; set; }
-        public string Address { get; set; }
+        public string? Name { get; set; }
+        public string? LastName { get; set; }
+        public string? Email { get; set; }
+        public string? UserName { get; set; }
+        public string? Hash { get; set; }
+        public string? Salt { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public bool Enabled { get; set; }
+        public Role Role { get; set; } = Role.User;
     }
 }
