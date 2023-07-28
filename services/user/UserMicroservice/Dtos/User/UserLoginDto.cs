2050b635086e4b7f074538963fe25c51b3b8ab86
Maxim P <raiyin@ya.ru>
2023-07-28 22:01:58 +0300

    CT-27 Added user dto

---
+++ services/user/UserMicroservice/Dtos/User/UserLoginDto.cs
@@ -0,0 +1,8 @@
+﻿namespace UserMicroservice.Dtos.User
+{
+    public class UserLoginDto
+    {
+        public string UserName { get; set; }=string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
