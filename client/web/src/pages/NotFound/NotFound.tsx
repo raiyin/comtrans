a4d1ad0983760a86957b87db7dd8b9b09dafad87
Maxim P. <raiyin@ya.ru>
2023-12-19 13:35:36 +0300

    CT-54 Not found page is done

--- client/web/src/pages/NotFound/NotFound.tsx
+++ client/web/src/pages/NotFound/NotFound.tsx
@@ -1,10 +1,34 @@
 import React from 'react'
+import cl from './notfound.module.scss';
+import { Box, Button } from '@mui/material';
+import { useNavigate } from 'react-router';
 
 const NotFound = () => {
+
+
+    const navigate = useNavigate();
+
     return (
-        <h1>
-            Not found
-        </h1>
+        <section className={cl['notfound']}>
+            <div className={cl['notfound-code']}>
+                404
+            </div>
+            <div className={cl["notfound-description"]}>
+                <span>Sorry, this page</span>
+                <span>isn't available!</span>
+
+                <Button
+                    key={'home'}
+                    onClick={() => navigate('/')}
+                    variant="contained"
+                    sx={{
+                        width: 300,
+                    }}
+                >
+                    Home
+                </Button>
+            </div>
+        </section>
     )
 }
 
